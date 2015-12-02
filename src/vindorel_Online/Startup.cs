c505b3558fc7f3fb09481f540e6e436ea4822ba6
canrcx <gurcaglarogulcan@gmail.com>
2015-12-02 19:09:31 +0200

    builder created

--- src/vindorel_Online/Startup.cs
+++ src/vindorel_Online/Startup.cs
@@ -5,20 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Configuration;
 
 namespace vindorel_Online
 {
+    public interface IFoo {  }
+    public  class Foo : IFoo { }
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        IConfiguration config;
+            public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile("config.json");
+            config = builder.Build();
+             
+
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+           services.AddTransient<IFoo,Foo>();
         }
 
         public void Configure(IApplicationBuilder app)
         {
             app.Run(async (context) =>
             {
+                
                 await context.Response.WriteAsync("Hello World!");
             });
         }
