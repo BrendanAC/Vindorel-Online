46b75ed6e061e3838df855a36adde6ca99eb1b31
canrcx <gurcaglarogulcan@gmail.com>
2015-12-07 22:21:28 +0200

    asp.net MVC project CREATED

---
+++ Vindorel_Online/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Vindorel_Online
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Vindorel_Online/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vindorel_Online.MvcApplication" Language="C#" %>
